88c48a667a2e0e2fb39fd98af7323d5e4c1a963b
root <root@foreman.am.local>
2019-01-16 15:37:28 +0300

    change model downloads

--- manifests/downloads.pp
+++ manifests/downloads.pp
@@ -3,15 +3,25 @@ class client_1c::downloads(
 	String   $ftp_server_name	= $::client_1c::ftp_server_name,
 	String   $file_suffix	        = $::client_1c::file_suffix,
 ){
-        include wget
+#        include wget
 	
-        wget::fetch { "$package_name_download":
-                      source             => "$ftp_server_name/$package_name_download",
-                      destination        => "/tmp/$package_name_download",
-                      timeout            => 0,
-                      verbose            => true,
-                      nocheckcertificate => true,
-	            }	
-         
+#        wget::fetch { "$package_name_download":
+#                	source             => "$ftp_server_name/$package_name_download",
+#                     	destination        => "/tmp/$package_name_download",
+#                     	timeout            => 0,
+#                      	verbose            => true,
+#                      	nocheckcertificate => true,
+#	        }	
+
+	archive { "/tmp/$package_name_download":
+			path          => "/tmp/${package_name_download}",
+			source        => "$ftp_server_name/$package_name_download",
+			ensure   	   => present,
+			extract            => true,
+			extract_command	   => 'tar xfvz %s ',
+			extract_path       => '/tmp',
+			cleanup            => true,
+		}
+
 }
 
